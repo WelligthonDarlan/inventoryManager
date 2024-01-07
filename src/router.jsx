8c76e1df016c6d3105d51c1f430f269e496266be
wellighon <welldarlan.pw@gmail.com>
2024-01-07 11:34:23 -0300

    configurações de deploy react-router-dom

--- src/router.jsx
+++ src/router.jsx
@@ -1,3 +1,32 @@
+// import { createBrowserRouter } from "react-router-dom";
+// import RootLayout from "./pages/RootLayout";
+// import ItemsLayout from "./pages/items/Layout";
+// import ListItems from "./pages/items/ListItems";
+// import CreateItem from "./pages/items/CreateItem";
+// import ShowItem from "./pages/items/ShowItem";
+// import UpdateItem from "./pages/items/UpdateItem";
+// import Home from "./pages/Home";
+
+// const router = createBrowserRouter([{
+//   path: "/",
+//   element: <RootLayout />,
+//   children: [
+//     { index: true, element: <Home /> },
+//     {
+//       path: "items",
+//       element: <ItemsLayout />,
+//       children: [
+//         { index: true, element: <ListItems /> },
+//         { path: "new", element: <CreateItem /> },
+//         { path: ":id", element: <ShowItem /> },
+//         { path: ":id/update", element: <UpdateItem /> }
+//       ]
+//     }
+//   ]
+// }])
+
+// export default router;
+
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from "./pages/RootLayout";
 import ItemsLayout from "./pages/items/Layout";
@@ -7,22 +36,26 @@ import ShowItem from "./pages/items/ShowItem";
 import UpdateItem from "./pages/items/UpdateItem";
 import Home from "./pages/Home";
 
-const router = createBrowserRouter([{
-  path: "/",
-  element: <RootLayout />,
-  children: [
-    { index: true, element: <Home /> },
-    {
-      path: "items",
-      element: <ItemsLayout />,
-      children: [
-        { index: true, element: <ListItems /> },
-        { path: "new", element: <CreateItem /> },
-        { path: ":id", element: <ShowItem /> },
-        { path: ":id/update", element: <UpdateItem /> }
-      ]
-    }
-  ]
-}])
+const repoName = "inventoryManager"; // Substitua pelo nome real do seu repositório
+
+const router = createBrowserRouter([
+  {
+    path: `/${repoName}`, // Adiciona o nome do repositório à rota base
+    element: <RootLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "items",
+        element: <ItemsLayout />,
+        children: [
+          { index: true, element: <ListItems /> },
+          { path: "new", element: <CreateItem /> },
+          { path: ":id", element: <ShowItem /> },
+          { path: ":id/update", element: <UpdateItem /> },
+        ],
+      },
+    ],
+  },
+]);
 
 export default router;
